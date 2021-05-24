bc603fad97ce9479104a4ccafdf30d4317dc8134
NixonXC <81410474+NixonXC@users.noreply.github.com>
2021-05-24 23:16:47 +0530

    Delete BAN.py

--- BAN.py
+++
@@ -1,5 +0,0 @@
-@client.command(aliases=['b'])
-@commands.has_permissions(ban_members = True)
-async def ban(ctx,member : discord.Member,*,reason= "Reason Not Provided"):
-  await ctx.send(member.name + " has Been BANNED from Server, Because: "+reason)
-  await member.ban(reason=reason)
